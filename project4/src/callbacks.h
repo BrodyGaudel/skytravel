41ddd237c06665d7e41b1b57a03fdcb03b58e03e
anis-saadaoui <anis.saadaoui@esprit.tn>
2019-12-13 01:24:42 -0800

    gestion des reclamations

---
+++ project4/src/callbacks.h
@@ -0,0 +1,55 @@
+#include <gtk/gtk.h>
+
+
+
+void
+on_creation___clicked                  (GtkWidget       *objet,
+                                        gpointer         user_data);
+
+void
+on_buttonanis2_clicked                 (GtkWidget       *objet,
+                                        gpointer         user_data);
+
+void
+on_buttonanis3_clicked                 (GtkWidget       *objet,
+                                        gpointer         user_data);
+
+void
+on_buttonanis4_clicked                 (GtkWidget       *objet,
+                                        gpointer         user_data);
+
+void
+on_buttonanis5_clicked                 (GtkWidget       *objet,
+                                        gpointer         user_data);
+
+void
+on_buttonanis8_clicked                 (GtkWidget       *objet,
+                                        gpointer         user_data);
+
+void
+on_buttonanis7_clicked                 (GtkWidget       *objet,
+                                        gpointer         user_data);
+
+void
+on_buttonanis14_clicked                (GtkWidget       *objet,
+                                        gpointer         user_data);
+
+void
+on_buttonanis9_clicked                 (GtkWidget       *objet,
+                                        gpointer         user_data);
+
+void
+on_buttonanis13_clicked                (GtkWidget       *objet,
+                                        gpointer         user_data);
+
+void
+on_buttonanis10_clicked                (GtkWidget       *objet,
+                                        gpointer         user_data);
+
+void
+on_buttonanis12_clicked                (GtkWidget       *objet,
+                                        gpointer         user_data);
+
+void
+on_buttonanis11_clicked                (GtkWidget       *objet,
+                                        gpointer         user_data);
